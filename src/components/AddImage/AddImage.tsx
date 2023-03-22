873000ec818e88e1a065148076eaa83958357d41
sylvain <sylvainmasclet24@gmail.com>
2023-03-22 14:38:49 +0200

    fixed overlay + drop and click image insert added

--- src/components/AddImage/AddImage.tsx
+++ src/components/AddImage/AddImage.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
 import { Image } from '../Image/Image';
 
 type Props = React.ImgHTMLAttributes<HTMLImageElement>;
 
 export const AddImage: React.FC<Props> = () => {
 
+
     /*
      * Goal: read the file inserted to extract the image that will be displayed
      *
      */
     const showImage = (src: any, target: any) => {
-        var fr = new FileReader();
+        var fr = new FileReader;
 
         fr.onload = function () {
             target.src = fr.result;
         }
-
         fr.readAsDataURL(src.files[0]);
     }
 
@@ -30,24 +29,34 @@ export const AddImage: React.FC<Props> = () => {
         showImage(src, target);
     }
 
+
     /*
-     * Goal: display an image inserted by the user
+     * Goal: display an image fetched by dragNdrop
      *
      */
-    function fetchImage() {
-        document.getElementById('select_image')!!.click();
+    function dropImage(e: any) {
+        var target = document.getElementById("target")!! as HTMLImageElement
+        var fr = new FileReader();
+
+        fr.onload = function (event) {
+            target.src = event.target!!.result as string;
+        }
+
+        fr.readAsDataURL(e.target.files[0]);
+        e.preventDefault();
     }
 
 
     return (
         <div className="add-image">
 
-            <Image id="target" className="choose-image" alt="insert image"
-                onClick={() => fetchImage()}
+            <Image id="target" className="choose-image" alt="add image"
+                onDrop={(event) => dropImage(event)}
             />
             <input type="file" id="select_image" className="disappear"
                 onChange={() => putImage()}
             />
         </div>
+
     );
 };
